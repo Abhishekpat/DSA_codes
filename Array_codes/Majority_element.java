8d91e611e02c7ea1863966eeff0a97265c1d169b
Abhishekpat <104193075+Abhishekpat@users.noreply.github.com>
2025-07-27 18:16:01 +0530

    Update Majority_element.java

--- Array_codes/Majority_element.java
+++ Array_codes/Majority_element.java
@@ -1,1 +1,26 @@
+//https://leetcode.com/problems/majority-element/description/
 
+
+public class Solution {
+
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3};  // New input
+        int result = majorityElement(nums);
+        System.out.println("Majority element is: " + result);
+    }
+}
